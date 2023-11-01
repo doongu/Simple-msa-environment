97120f7a72a6b5d4a928e282979fc556ac2bc8a5
doongu <must1080@naver.com>
2023-11-01 21:32:24 +0900

    간단한 api 추가

---
+++ src/main/java/com/example/demo/controller/firstController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RestController
+@RequiredArgsConstructor
+public class firstController {
+
+    @RequestMapping("/restTest")
+    public String restTest(@RequestParam String str){
+        return str + " : Rest Test 완료!!!";
+    }
+}
